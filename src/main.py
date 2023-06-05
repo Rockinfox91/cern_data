83a3dc6533630052c1548892fe98df99c200961b
Lucas BOISTAY <lucasboistay@hotmail.fr>
2023-06-05 23:22:20 +0200

    Ajout get_easy_graph
    Affichage simple d'un graphique ou de 4 graphiques utilisant la même donnée d'abcisse

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -21,7 +21,35 @@ def lire_data(data_file_name):
     return df
 
 
+def get_easy_graph(df, colx, coly):
+    if isinstance(coly, str):
+        coly = [coly]
+
+    num_plots = len(coly)
+    fig, axes = plt.subplots(2, 2, figsize=(10, 10), squeeze=False)
+    axes = axes.flatten()
+
+    for i in range(num_plots):
+        ax = axes[i]
+        ax.plot(df[colx], df[coly[i]])
+        ax.set_xlabel(colx)
+        ax.set_ylabel(coly[i])
+
+    if num_plots == 1:
+        fig.suptitle(f"Graphe de {coly[0]} par rapport à {colx}")
+    else:
+        fig.suptitle("Graphique des températures des points\nA, B, C et D en fonction du temps")
+        for i, col in enumerate(coly):
+            axes[i].set_title(col)
+
+    plt.tight_layout()
+    plt.show()
+
+
 if __name__ == "__main__":
     print("----- Début programme -----")
     data = lire_data("cern_data_run10")
     print(data)
+
+    print("Graphe simple de Ta en fonction du temps")
+    get_easy_graph(data, "time", ["Ta","Tb","Tc","Td"])
